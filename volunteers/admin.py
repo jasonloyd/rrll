3dfae11b6f33d2eada9eb9dedcfd7a9c7237f836
Jason Loyd <the.jason.loyd@gmail.com>
2024-03-01 12:02:43 -0700

    admin page formatting

--- volunteers/admin.py
+++ volunteers/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 
 from .models import Volunteer
 
-admin.site.register(Volunteer)
+
+class VolunteerAdmin(admin.ModelAdmin):
+    list_display = [
+        "first_name",
+        "middle_name",
+        "last_name",
+        "email_address",
+        "volunteer_role",
+        "background_check_status",
+        "created_at",
+    ]
+    list_filter = ["created_at", "background_check_status"]
+    search_fields = ["first_name", "last_name"]
+
+
+admin.site.register(Volunteer, VolunteerAdmin)
